a2534fee5144e37a063faa6c4d276d63666a13be
Ivan Tavarez <ivantavarez2@gmail.com>
2023-06-22 18:33:24 -0400

    Working with Stacks

---
+++ src/main/java/com/itavare0/WorkingWithStacks.java
@@ -0,0 +1,13 @@
+package com.itavare0;
+
+import java.util.Stack;
+
+public class WorkingWithStacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.peek());
+    }
+}
